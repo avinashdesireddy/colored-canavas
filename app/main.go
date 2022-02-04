8e80ade4bbf9041feb4c2456951debf53e1fbcbb
Avinash Desireddy <avinashr.desireddy@gmail.com>
2022-02-04 16:16:40 -0600

    initial version

---
+++ app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "html/template"
+  "log"
+  "net/http"
+  "os"
+)
+
+type PageVariables struct {
+  Color string
+  Title string
+  Description string
+}
+
+func main() {
+  http.HandleFunc("/", HomePage)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request){
+  HomePageVars := PageVariables{
+    Color: os.Getenv("COLOR"),
+    Title: os.Getenv("TITLE"),
+    Description: os.Getenv("DESCRIPTION"),
+  }
+  t, err := template.ParseFiles("homepage.html")
+  if err != nil {
+    log.Print("Template parsing error: ", err)
+  }
+  err = t.Execute(w, HomePageVars)
+  if err != nil {
+    log.Print("Template executing error: ", err)
+  }
+}
